1c9b4bf16e3451ddd7ec62581274e822ecfd09dd
Dayz221 <rutuchenal37@gmail.com>
2024-03-05 17:51:42 +0300

    п

--- src/girls.js
+++ src/girls.js
@@ -2,22 +2,6 @@ export const list =
 [
     {
         id: 0,
-        name: "Мама Егора",
-        photos: [
-            "https://img.championat.com/c/1200x900/news/big/p/f/eva-elfi-sdelala-vybor-mezhdu-ronaldu-i-messi_167100744948259179.jpg",
-            "https://the-flow.ru/uploads/images/resize/830x0/adaptiveResize/14/43/28/86/29/475425240cd7.jpg",
-            "https://newsomsk.ru/images/news/fullhd/2022/01/beabe580adc212da29ff496ab46de503.jpg"
-        ],
-        count: 3,
-        comments: [
-            "Сколько стоит?",
-            "Егор, я твою маму нашел",
-            "#НаташинскаяВпадина",
-            "#3МетраПодЗемлей",
-        ]
-    },
-    {
-        id: 1,
         name: "ИМЯ2",
         photos: [
             "https://the-flow.ru/uploads/images/resize/830x0/adaptiveResize/14/43/28/86/29/475425240cd7.jpg",
@@ -34,7 +18,7 @@ export const list =
         ]
     },
     {
-        id: 2,
+        id: 1,
         name: "ИМЯ3",
         photos: [
             "https://newsomsk.ru/images/news/fullhd/2022/01/beabe580adc212da29ff496ab46de503.jpg"
